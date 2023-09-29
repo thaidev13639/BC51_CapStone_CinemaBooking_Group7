ac0a9dcff3da357183eeab3347deebc7e35b73a8
HoangAnh <hoanganh51199@gmail.com>
2023-09-30 01:14:14 +0700

    lan 6 +movidedetail

--- src/services/movie.js
+++ src/services/movie.js
@@ -11,8 +11,24 @@ class MovieService {
         });
 
     }
-    fetchMovieDetailApi() {
-
+    
+    fetchMovieBannerApi() {
+        return axios({
+            url: "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachBanner",
+            method: "GET",
+            headers: {
+                TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MSIsIkhldEhhblN0cmluZyI6IjIzLzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwODY0NjQwMDAwMCIsIm5iZiI6MTY4MDM2ODQwMCwiZXhwIjoxNzA4Nzk0MDAwfQ.m054V9MFrBY26j2t-FxqIXGcOVQim2UUk_G-OoewJUY"
+            },
+        });
+    }
+    fetchMovieTabApi() {
+        return axios({
+            url: "https://movienew.cybersoft.edu.vn/api/QuanLyRap/LayThongTinHeThongRap",
+            method: "GET",
+            headers: {
+                TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MSIsIkhldEhhblN0cmluZyI6IjIzLzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwODY0NjQwMDAwMCIsIm5iZiI6MTY4MDM2ODQwMCwiZXhwIjoxNzA4Nzk0MDAwfQ.m054V9MFrBY26j2t-FxqIXGcOVQim2UUk_G-OoewJUY"
+            },
+        });
     }
 }
 
--- src/sticky.js
+++ src/sticky.js
@@ -12,12 +12,12 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").classList.add("bg-light");
+    document.getElementById("header").classList.add("bg-white");
     //  toggleBtn.style.color="black";
     document.getElementById("dropDown").classList.remove("text-white");
     document.getElementById("logo").style.color = "black";
   } else {
-    document.getElementById("header").classList.remove("bg-light");
+    document.getElementById("header").classList.remove("bg-white");
     //  toggleBtn.style.color="white";
     document.getElementById("dropDown").classList.add("text-white");
     document.getElementById("logo").style.color = "#fff";
