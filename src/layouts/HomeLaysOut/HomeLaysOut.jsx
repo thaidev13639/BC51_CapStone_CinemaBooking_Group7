6fea585b696eb96b734a61011711e7b2657d1e25
thaidev13639 <pnt0964413557@gmail.com>
2023-10-07 21:07:35 +0700

    update scroll

--- src/layouts/HomeLaysOut/HomeLaysOut.jsx
+++ src/layouts/HomeLaysOut/HomeLaysOut.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Headers from "../../components/Header/Headers";
 import Footer from "../../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import "../../css/style.css";
-import "../../sticky.js";
+// import "../../sticky.js";
 
 export default function HomeLaysOut() {
+  useEffect(() => {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        document.getElementById("header").classList.add("bg-white");
+
+        document.getElementById("dropDown").classList.remove("text-white");
+        document.getElementById("logo").style.color = "black";
+      } else {
+        document.getElementById("header").classList.remove("bg-white");
+        document.getElementById("dropDown").classList.add("text-white");
+        document.getElementById("logo").style.color = "#fff";
+      }
+    }
+  }, []);
   return (
     <>
       <Headers />
--- src/sticky.js
+++ src/sticky.js
@@ -9,16 +9,16 @@
 //         body.classList.remove("dark");
 // }
 // }
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").classList.add("bg-white");
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("header").classList.add("bg-white");
  
-    document.getElementById("dropDown").classList.remove("text-white");
-    document.getElementById("logo").style.color = "black";
-  } else {
-    document.getElementById("header").classList.remove("bg-white");
-    document.getElementById("dropDown").classList.add("text-white");
-    document.getElementById("logo").style.color = "#fff";
-  }
-}
+//     document.getElementById("dropDown").classList.remove("text-white");
+//     document.getElementById("logo").style.color = "black";
+//   } else {
+//     document.getElementById("header").classList.remove("bg-white");
+//     document.getElementById("dropDown").classList.add("text-white");
+//     document.getElementById("logo").style.color = "#fff";
+//   }
+// }
